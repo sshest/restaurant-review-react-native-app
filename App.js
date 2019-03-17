fce624b66eef17da94d360b6e8fef86655d4e7b9
sergii.shestakov <sergii.shestakov@dev-pro.net>
2019-03-17 22:28:07 +0200

    Accept user input with React native TextInput

--- App.js
+++ App.js
@@ -1,48 +1,71 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 import HeaderStyle from './HeaderStyle'
 
 const restaurants = [{
-    name: 'React Cafe', address: '123 Anywhere str.'
-}, {
-    name: 'Funny restaurant', address: '456 Elsewhere str.'
-}];
+        name: 'React Cafe', address: '123 Anywhere str.'
+    }, {
+        name: 'Funny restaurant', address: '456 Elsewhere str.'
+    }, {
+        name: 'Tacos', address: '22 Doctor Negrin str.'
+    }];
 
 export default class App extends Component {
+    state = {
+        search: null
+    };
 
-  render() {
-    return (
-      <View style={{
-          flex: 1
-      }}>
-        <Text style={HeaderStyle.header}>Restaurant Review</Text>
-          {
-              restaurants.map((place, index) => {
-                  return (
-                      <View key={place.name}
-                      style={[
-                          styles.row,
-                          {
-                              backgroundColor: index % 2 === 0 ? 'white': '#F3F3F7'
-                          }
-                      ]}>
-                          <View style={styles.edges}>
-                              <Text>{index + 1}</Text>
-                          </View>
-                          <View style={styles.nameAddress}>
-                              <Text>{place.name}</Text>
-                              <Text style={styles.addressText}>{place.address}</Text>
-                          </View>
-                          <View style={styles.edges}><Text>Info</Text>
-                          </View>
-                      </View>
-                  )
-              })
-          }
-      </View>
-    );
-  }
+      render() {
+        return (
+          <View style={{
+              flex: 1
+          }}>
+            <Text style={HeaderStyle.header}>Restaurant Review</Text>
+              <TextInput
+                  style={
+                      styles.input
+                  }
+                  placeholder="Live Search"
+                  onChangeText={
+                    text => {
+                      this.setState({
+                          search: text
+                      })
+                    }
+                  }
+              />
+              {
+                  restaurants
+                      .filter(place => {
+                          return !this.state.search
+                              || place.name.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1;
+                      })
+                      .map((place, index) => {
+                          return (
+                              <View key={place.name}
+                                  style={[
+                                      styles.row,
+                                      {
+                                          backgroundColor: index % 2 === 0 ? 'white': '#F3F3F7'
+                                      }
+                                  ]}>
+                                      <View style={styles.edges}>
+                                          <Text>{index + 1}</Text>
+                                      </View>
+                                      <View style={styles.nameAddress}>
+                                          <Text>{place.name}</Text>
+                                          <Text style={styles.addressText}>{place.address}</Text>
+                                      </View>
+                                      <View style={styles.edges}><Text>Info</Text>
+                                      </View>
+                                  </View>
+                              )
+                          })
+              }
+          </View>
+        );
+      }
 }
 
 const styles = StyleSheet.create({
@@ -61,5 +84,15 @@ const styles = StyleSheet.create({
     },
     addressText: {
         color: 'grey'
+    },
+    input: {
+        marginBottom: 30,
+        padding: 10,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        borderBottomWidth: 1,
+        borderColor: '#ddd',
+        backgroundColor: '#F5F5F5'
     }
 });
