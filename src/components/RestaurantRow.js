23f5166f1d26a0773ab28d498ee85f96ad7d902c
sergii.shestakov <sergii.shestakov@dev-pro.net>
2019-03-18 00:03:03 +0200

    Make a custom Touchable Button

--- src/components/RestaurantRow.js
+++ src/components/RestaurantRow.js
@@ -1,7 +1,24 @@
 import  React, {Component} from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import {
+    Text,
+    TouchableOpacity,
+    TouchableHighlight,
+    TouchableWithoutFeedback,
+    StyleSheet,
+    View
+} from 'react-native';
 
 export default class RestaurantRow extends Component {
+    state = {
+        showInfo: false
+    };
+
+    infoPressed = () => {
+        this.setState({
+            showInfo: !this.state.showInfo
+        })
+    };
+
     render() {
         const {
             place,
@@ -10,22 +27,30 @@ export default class RestaurantRow extends Component {
 
         return (
             <View key={place.name}
-                  style={[
-                      styles.row,
-                      {
-                          backgroundColor: index % 2 === 0 ? 'white': '#F3F3F7'
-                      }
-                  ]}>
-                <View style={styles.edges}>
-                    <Text>{index + 1}</Text>
+                  style={{backgroundColor: index % 2 === 0 ? 'white': '#F3F3F7'}}>
+                <View style={styles.row}>
+                    <View style={styles.edges}>
+                        <Text>{index + 1}</Text>
+                    </View>
+                    <View style={styles.nameAddress}>
+                        <Text>{place.name}</Text>
+                        <Text style={styles.addressText}>{place.address}</Text>
+                    </View>
+                    <View style={styles.edges}>
+                    <TouchableOpacity
+                        onPress={this.infoPressed}
+                        style={styles.button}
+                    >
+                        <Text style={styles.buttonText}>Info</Text>
+                    </TouchableOpacity>
+                    </View>
                 </View>
-                <View style={styles.nameAddress}>
-                    <Text>{place.name}</Text>
-                    <Text style={styles.addressText}>{place.address}</Text>
-                </View>
-                <View style={styles.edges}>
-                    <Text>Info</Text>
-                </View>
+                {
+                    this.state.showInfo &&
+                    <View>
+                        <Text>Restaurant Info</Text>
+                    </View>
+                }
             </View>
         )
     }
@@ -39,7 +64,8 @@ const styles = StyleSheet.create({
         flex:1,
         alignItems: 'center',
         justifyContent: 'center',
-        padding: 5
+        padding: 5,
+        minWidth: 50
     },
     nameAddress: {
         flexDirection:'column',
@@ -48,4 +74,16 @@ const styles = StyleSheet.create({
     addressText: {
         color: 'grey'
     },
+    button: {
+        borderWidth: 1,
+        borderColor: '#0066CC',
+        borderRadius: 14,
+        paddingHorizontal: 10,
+        paddingVertical: 3,
+        backgroundColor: '#FFF'
+    },
+    buttonText: {
+        color: '#0066CC',
+        fontSize: 12
+    }
 })
